7303b24264a138a52dff590c78960547a807eb09
ppiter <ikeike@yandex.ru>
2025-08-09 17:23:51 +0300

    fix: wip

--- jenkinsFromDocker/runDocker.sh
+++ jenkinsFromDocker/runDocker.sh
@@ -1,1 +1,1 @@
-docker run -p 8080:8080 -p 50000:50000 -v jenkins_home:/jenkins_home jenkins/jenkins:lts-jdk11
+docker run -p 8080:8080 -p 50000:50000 -v $PWD/jenkins_home:/jenkins_home jenkins/jenkins:lts-jdk11
